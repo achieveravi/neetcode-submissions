a18ac9bb5b489b7b9b084be9be7793bf643457e8
Ravi Mishra <ravi.achieve@gmail.com>
2026-04-18 04:11:50 +0000

    removing irrelevant submissions

--- Data Structures & Algorithms/two-integer-sum/submission-4.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            complimentaryNum = target - nums[i]
-            remainingListStartIdx = i + 1
-            remainingList = nums[remainingListStartIdx:]
-            if complimentaryNum in remainingList:
-                return [i, remainingListStartIdx + remainingList.index(complimentaryNum)]
-        return []
